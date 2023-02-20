6fca39bbea1cc58e362c326d167b346038304211
mildosky <ibrahimmusah205@gmail.com>
2023-02-20 08:45:49 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H
+#define _SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -13,37 +14,30 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
 
+/* print.c */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
 
 void bubble_sort(int *array, size_t size);
-int len_list(listint_t *h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_recursion(int *array, int left, int right, size_t size);
-int partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
-void switch_nodes(listint_t **list, listint_t **p);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge_recursion(int *arr, int *array, size_t left, size_t right);
-void merge_subarray(int *arr, int *array, size_t left,
-		size_t middle, size_t right);
-void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
-void count_sort_LSD(int *array, size_t size, size_t lsd);
-void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
-void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
-void bitonic_merge(int *array, int l, int r, int direction);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
