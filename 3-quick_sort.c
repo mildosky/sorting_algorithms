2595212560cd295c77fc294e4de2e83ff6bdc515
mildosky <ibrahimmusah205@gmail.com>
2023-02-20 08:44:20 +0100

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,76 +1,95 @@
 #include "sort.h"
 
+void divide(int beg, int pivot, int *i, size_t size);
+int partition(int beg, int pivot, int *i, size_t size);
+void swap_int(int *a, int *b);
 /**
- * swap - swaps 2 int values
- * @array: the integer array to sort
- * @size: the size of the array
- * @a: address of first value
- * @b: address of second value
- *
- * Return: void
- */
-void swap(int *array, size_t size, int *a, int *b)
-{
-	if (*a != *b)
-	{
-		*a = *a + *b;
-		*b = *a - *b;
-		*a = *a - *b;
-		print_array((const int *)array, size);
-	}
-}
-
-/**
- * lomuto_partition - partitions the array
- * @array: the integer array to sort
- * @size: the size of the array
- * @lo: the low index of the sort range
- * @hi: the high index of the sort range
- *
- * Return: void
- */
-size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
-{
-	int i, j, pivot = array[hi];
-
-	for (i = j = lo; j < hi; j++)
-		if (array[j] < pivot)
-			swap(array, size, &array[j], &array[i++]);
-	swap(array, size, &array[i], &array[hi]);
-
-	return (i);
-}
-
-/**
- * quicksort - quicksorts via Lomuto partitioning scheme
- * @array: the integer array to sort
- * @size: the size of the array
- * @lo: the low index of the sort range
- * @hi: the high index of the sort range
- *
- * Return: void
- */
-void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
-{
-	if (lo < hi)
-	{
-		size_t p = lomuto_partition(array, size, lo, hi);
-
-		quicksort(array, size, lo, p - 1);
-		quicksort(array, size, p + 1, hi);
-	}
-}
-
-/**
- * quick_sort - calls quicksort
- * @array: the integer array to sort
- * @size: the size of the array
- *
- * Return: void
+ * quick_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of the array
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || !size)
-		return;
-	quicksort(array, size, 0, size - 1);
+	int beg = 0, pivot;
+
+	if (array && size > 1)
+	{
+		pivot = (size - 1);
+		divide(beg, pivot, array, size);
+	}
+}
+/**
+* divide - recursively partition
+* @beg: beginning of divided array
+* @pivot: end of divided array
+* @i: the beginning of the array
+* @size: size of array
+**/
+void divide(int beg, int pivot, int *i, size_t size)
+{
+	int first, second, np;
+
+	if (beg < pivot)
+	{
+		second = partition(beg, pivot, i, size);
+		first = beg;
+		np = second - 1;
+		if (first != np && second != pivot)
+			np--;
+		divide(first, np, i, size);
+		divide(second, pivot, i, size);
+	}
+}
+/**
+* partition - divides an array
+* @beg: beginning of array separated
+* @pivot: end of array separated
+* @i: the beginning of array
+* @size: size of array
+* Return: the new beginning
+**/
+int partition(int beg, int pivot, int *i, size_t size)
+{
+	int temp;
+
+	temp = beg;
+	while (temp != pivot)
+	{
+		if (i[temp] < i[pivot])
+		{
+			if (temp != beg)
+			{
+				swap_int(i + temp, i + beg);
+				print_array(i, size);
+			}
+			temp++;
+			beg++;
+		}
+		else
+			temp++;
+	}
+	if (beg != pivot)
+	{
+		if (i[beg] > i[pivot])
+		{
+			swap_int(i + pivot, i + beg);
+			print_array(i, size);
+		}
+		beg++;
+	}
+	return (beg);
+}
+
+/**
+  * swap_int - swaps the values of two integers
+  * @a: take an int
+  * @b: take an int
+  */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
